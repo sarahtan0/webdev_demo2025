93adf0d4a8e8c68f3fabd38db6080291ee344b26
Sarah <tans9@uci.edu>
2025-01-08 22:56:45 -0800

    moved travel to another file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 import AboutMe from './AboutMe.jsx'
 import Home from './Home.jsx'
+import Travel from './Travel.jsx'
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 
@@ -12,11 +13,13 @@ function App() {
         <nav className="bg-blue-100 flex gap-3" >
           <Link to="/">HOME</Link>
           <Link to="/about">ABOUT ME</Link>
+          <Link to="/travel">TRAVEL</Link>
         </nav>
       </div>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="about" element={<AboutMe/>}/>
+        <Route path="travel" element={<Travel/>}/>
       </Routes>
     </BrowserRouter>
   )
